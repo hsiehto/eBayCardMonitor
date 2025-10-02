6542ac0a0d7874c122908dc8c9cd5431da38e2cc
Tom Hsieh <tomhsieh@Toms-Mac-mini-1.attlocal.net>
2025-10-01 23:01:10 -0700

    initial text field and search button setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState("");
+  const handleSearch = () => {
+    console.log(searchValue);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TextField
+        id="standard-basic"
+        label="search an item"
+        variant="standard"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <Button variant="contained" color="success" onClick={handleSearch}>
+        search
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
