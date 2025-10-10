b56d305476076281cd98e8b4cab6311c00fe4e7f
hsiehto <hsieh.tom@gmail.com>
2025-10-10 14:19:30 -0700

    create backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
-  const handleSearch = () => {
+  const handleSearch = async () => {
     console.log(searchValue);
+    const url = `http://localhost:5000/api/search?query=${searchValue}`;
+    try {
+      const response = await fetch(url);
+      console.log(response);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
