1c542c4dcbca4e3f1ae264289af036b6ce5ed060
hsiehto <hsieh.tom@gmail.com>
2025-10-15 21:14:31 -0700

    display api results

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const handleSearch = async () => {
     console.log(searchValue);
     const url = `http://localhost:5000/api/search?query=${searchValue}`;
     try {
       const response = await fetch(url);
-      console.log(response);
       const result = await response.json();
-      console.log(result);
+      console.log(result.itemSummaries);
+
+      setSearchResults(result.itemSummaries);
     } catch (error) {
       console.error(error.message);
     }
@@ -27,6 +34,28 @@ function App() {
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
       />
+      {searchResults.map((item, index) => {
+        return (
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.image.imageUrl}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {`${item.price.currency} ${item.price.value}`}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        );
+      })}
       <Button variant="contained" color="success" onClick={handleSearch}>
         search
       </Button>
