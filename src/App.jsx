7f9517c82401c6d91353fa0907fda855c48ee769
Tom Hsieh <hsieh.tom@gmail.com>
2025-10-20 21:15:38 -0700

    Added error message and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,68 +7,83 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
-import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const handleSearch = async () => {
     console.log(searchValue);
     const url = `http://localhost:5000/api/search?query=${searchValue}`;
     try {
       const response = await fetch(url);
       const result = await response.json();
-      console.log(result.itemSummaries);
-
-      setSearchResults(result.itemSummaries);
+      if (result?.error?.errors[0]?.message) {
+        setErrorMessage(result.error.errors[0].message);
+      } else {
+        console.log("check");
+        setErrorMessage("");
+        setSearchResults(result.itemSummaries);
+      }
     } catch (error) {
-      console.error(error.message);
+      console.error("error message", error.message);
     }
   };
 
   return (
     <>
-      <div className="search-container">
-        <TextField
-          id="standard-basic"
-          label="search an item"
-          variant="standard"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <Button variant="contained" color="success" onClick={handleSearch}>
-          search
-        </Button>
-      </div>
-      <div className="flex-container">
-        {searchResults.map((item) => {
-          return (
-            <a href={item.itemWebUrl} key={item.itemId} target="_blank">
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={item.image.imageUrl}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {item.title}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{ color: "text.secondary" }}
-                    >
-                      {`${item.price.currency} ${item.price.value}`}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </a>
-          );
-        })}
-      </div>
+      <Container fixed>
+        <div className="search-container">
+          <TextField
+            id="standard-basic"
+            label="search an item"
+            variant="standard"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <Button variant="contained" color="success" onClick={handleSearch}>
+            search
+          </Button>
+        </div>
+        <div className="flex-container">
+          <div>{errorMessage}</div>
+          {!searchResults.length && <div>No search results</div>}
+          {searchResults &&
+            searchResults.map((item) => {
+              return (
+                <a
+                  href={item.itemWebUrl}
+                  key={item.itemId}
+                  target="_blank"
+                  className="flex-item"
+                >
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={item.image.imageUrl}
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h6" component="div">
+                          {item.title}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          sx={{ color: "text.secondary" }}
+                        >
+                          {`${item.price.currency} ${item.price.value}`}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </a>
+              );
+            })}
+        </div>
+      </Container>
     </>
   );
 }
